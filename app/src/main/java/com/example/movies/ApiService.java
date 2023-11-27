602d4540687fa51b63e4d19e0fd983974dad7df5
raine <rainey888@mail.ru>
2023-11-28 00:00:38 +0700

    - Added trailers.

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -2,10 +2,14 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("v1.4/movie?token=HCFZECX-J6BMCS0-NF9GDT5-MM894BN&rating.kp=7-10&sortField=votes.kp&sortType=-1&limit=30")
+    @GET("movie?token=HCFZECX-J6BMCS0-NF9GDT5-MM894BN&rating.kp=7-10&sortField=votes.kp&sortType=-1&limit=30")
     Single<MovieResponse> loadMovies(@Query("page") int page);
+
+    @GET("movie/{idFilms}?token=HCFZECX-J6BMCS0-NF9GDT5-MM894BN")
+    Single<TrailerResponse> loadTrailers(@Path("idFilms") int idFilms);
 }
