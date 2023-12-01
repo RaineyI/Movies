b137260b942c30f97b907c63306de1d7a5009cdd
raine <rainey888@mail.ru>
2023-12-01 13:30:38 +0700

    - Added viewing of trailers with browser

--- app/src/main/java/com/example/movies/TrailersAdapter.java
+++ app/src/main/java/com/example/movies/TrailersAdapter.java
@@ -14,10 +14,15 @@ import java.util.List;
 public class TrailersAdapter extends RecyclerView.Adapter<TrailersAdapter.trailerViewHolder> {
 
     private List<Trailer> trailers = new ArrayList<>();
+    private OnTrailerClickListener onTrailerClickListener;
 
     public void setTrailers(List<Trailer> trailers) {
         this.trailers = trailers;
         notifyDataSetChanged();
+    }
+
+    public void setOnTrailerClickListener(OnTrailerClickListener onTrailerClickListener) {
+        this.onTrailerClickListener = onTrailerClickListener;
     }
 
     @NonNull
@@ -35,12 +40,24 @@ public class TrailersAdapter extends RecyclerView.Adapter<TrailersAdapter.traile
     public void onBindViewHolder(@NonNull trailerViewHolder holder, int position) {
         Trailer trailer = trailers.get(position);
         holder.textViewTrailer.setText(trailer.getName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(onTrailerClickListener != null) {
+                    onTrailerClickListener.onTrailerClick(trailer);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return trailers.size();
     }
+
+interface OnTrailerClickListener {
+        void onTrailerClick(Trailer trailer);
+}
 
     static class trailerViewHolder extends RecyclerView.ViewHolder {
 
